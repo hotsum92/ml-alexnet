7ab0af8c6574fb494c4e3cb5bed4c4981a76dce9
hotsum <hkawashimajob1992@gmail.com>
2024-11-02 18:00:42 +0900

    add alexnet.py

---
+++ alexnet.py
@@ -0,0 +1,38 @@
+import torch
+
+class AlexNet(torch.nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.features = nn.Sequential(
+            torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.BatchNorm2d(192),
+            torch.nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.BatchNorm2d(256),
+        )
+        self.classifier = nn.Sequential(
+            torch.nn.Dropout(p=0.5),
+            torch.nn.Linear(256 * 4 * 4, 4096),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Dropout(p=0.5),
+            torch.nn.Linear(4096, 4096),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
